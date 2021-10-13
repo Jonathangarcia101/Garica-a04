e83c7377807300411f51aff340682746c538bf3c
Jonathan Garica <Kingcub101@gmail.com>
2021-10-13 14:51:39 -0400

    first take and some structure

---
+++ src/main/java/baseline/solution41.java
@@ -0,0 +1,48 @@
+package baseline;
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Jonathan Garcia
+ */
+//Create a program that reads in the following list of names from a file called `exercise41_input.txt` and sorts the list alphabetically:
+//Create a file called "exercise41_input.txt" with a list of names for testing 
+//Create a file called "exercise41_output.txt" that will store the sorted list.
+//write a function called name_sorter to sort the names alphabetically
+//call the file "exercise41_input.txt" to name_Sorter
+// let name_Sorter count the number of elements in the list and alphabetically sort the names.
+// send the sorted names to file "exercise41_output.txt"
+// print how many names there are and list them.
+//done?
+//example
+/*Ling, Mai
+Johnson, Jim
+Zarnecki, Sabrina
+Jones, Chris
+Jones, Aaron
+Swift, Geoffrey
+Xiong, Fong
+Then print the sorted list to a file called `exercise41_output.txt` that looks like the following example output.
+
+Example Output
+Total of 7 names
+-----------------
+Johnson, Jim
+Jones, Aaron
+Jones, Chris
+Ling, Mai
+Swift, Geoffrey
+Xiong, Fong
+Zarnecki, Sabrina
+*/
+//Constraint
+//Don't hard-code the number of names.
+public class solution41 {
+    public static void main(String[] args) {
+        name_sorter();
+    }
+
+    private static void name_sorter() {
+
+        return null;
+
+    }
+}
